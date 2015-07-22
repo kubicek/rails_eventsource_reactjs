70512cde43c63ea07f423ae912adb589a5b42e19
Jiří Kubíček <jiri@kubicek.cz>
2015-07-22 20:31:58 +0200

    Adding reds

---
+++ config/initializers/notifier.rb
@@ -0,0 +1,10 @@
+module Notifier
+  class << self
+    def redis
+      @redis ||= Redis.new(:url => (ENV["REDIS_URL"] || 'redis://127.0.0.1:6379'))
+    end
+    def publish(queue, msg)
+      redis.publish queue, msg
+    end
+  end
+end
